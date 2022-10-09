475a47f91b33079f027a0d11b5393bd597537bd7
zunzum <zunzum@naver.com>
2022-10-09 12:37:21 +0900

    Adapter 뼈대작업 마무리

--- app/src/main/java/com/zunzum/listviewpractice_zun/adapters/StudentAdapter.kt
+++ app/src/main/java/com/zunzum/listviewpractice_zun/adapters/StudentAdapter.kt
@@ -1,11 +1,29 @@
 package com.zunzum.listviewpractice_zun.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.zunzum.listviewpractice_zun.R
 import com.zunzum.listviewpractice_zun.da.Student
 
 class StudentAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
+
 }
